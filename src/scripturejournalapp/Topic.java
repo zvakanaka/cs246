282216428f37884eec78bc5f49ff78cba15f5b06
Adam Quinton <quintonish@gmail.com>
2016-02-02 18:45:58 -0700

    Topics now have comparison operators to prevent duplicates

--- src/scripturejournalapp/Topic.java
+++ src/scripturejournalapp/Topic.java
@@ -35,6 +35,33 @@ public class Topic {
         return this.topic;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Topic other = (Topic) obj;
+        if ((this.subTopic == null) ? (other.subTopic != null) : !this.subTopic.equals(other.subTopic)) {
+            return false;
+        }
+        if ((this.topic == null) ? (other.topic != null) : !this.topic.equals(other.topic)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (this.subTopic != null ? this.subTopic.hashCode() : 0);
+        hash = 53 * hash + (this.topic != null ? this.topic.hashCode() : 0);
+        return hash;
+    }
+
     public Boolean isTopic(String t) {
         return Index.getIndex().isTopic(t);
     }
